cfa5ae4fb0ee5a5d99ba44adf76f7c7036ef2f38
John Sison <johnk.sison@gmail.com>
2019-03-13 19:48:55 -0400

    filter http request by /admin

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ app.get('/favicon.ico', (req, res) => {
     res.status(204);
 });
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>');
