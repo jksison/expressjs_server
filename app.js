925ee78bd0471e6a4c6b9e901d3d281dbe5d377b
John Sison <johnk.sison@gmail.com>
2019-03-12 20:55:54 -0400

    handling different routes

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Hello 1');
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
     console.log('Hello 2');
-    res.send();
+    res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
