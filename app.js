39738d174295db658e3ee98affceaed30a11fd2c
John Sison <johnk.sison@gmail.com>
2019-03-13 18:57:56 -0400

    body-parser extended set to true when parsing html body

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
-
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('Hello 2');
     res.send('<h1>Hello from Express!</h1>');
 });
 
