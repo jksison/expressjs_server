37e6fc32414d15ac2f34d4cc0ab0dd1e3b43a541
John Sison <johnk.sison@gmail.com>
2019-03-13 19:40:18 -0400

    added error page

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.get('/favicon.ico', (req, res) => {
+    res.status(204);
+});
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
