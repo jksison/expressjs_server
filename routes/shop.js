8c9588cafde80f848ecd86bdd56d2f5572b0864f
John Sison <johnk.sison@gmail.com>
2019-03-13 20:49:01 -0400

    serve add-product page and shop page

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 //express.Router is a mini express
 const router = express.Router();
 
+// __dirname is absolute path to this file
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+    res.sendFile(path.join(__dirname,'../','views','shop.html')); // backward slash in linux, forward slash in windows
 });
 
 
